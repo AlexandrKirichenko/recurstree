ccbf6ef6cb828a49d7569466e50996fd61b2124d
easybrizi <kiri4enkoalexandr@gmail.com>
2022-07-17 10:10:46 +0300

    add useState Notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
-function App() {
+const root = {
+  id: "1",
+  children: [{
+    id: "2",
+    children: [{
+      id: "3",
+      children: [{
+        id: "4"
+      },
+        {
+          id: "5"
+        },
+        {
+          id: "6"
+        },
+        {
+          id: "7",
+          children: [{
+            id: "77"
+          }]
+        }]
+    }]
+  },
+    {
+      id: "8",
+      children: [{
+        id: "9",
+      },
+        {
+          id: "10"
+        }]
+    }]
+}
+
+const Notes = ({ id, children }) => {
+  // const [showChildren, setShowChildren] = React.useState(true);
+  // const handleClick = React.useCallback(() => {
+  //   setShowChildren(!showChildren);
+  // }, [showChildren, setShowChildren])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {/*<span onClick={handleClick}>*/}
+      
+      {/*</span>*/}
+      <li>
+        <h4 >{id}</h4>
+        {(children ?? []).map((node) => <Notes {...node}/>)}
+      </li>
+    </ul>
+  )
+}
+
+function App() {
+  const [notes,setNotes] = useState([]);
+  return (
+    <div>
+      <Notes {...root}/>
     </div>
   );
 }
